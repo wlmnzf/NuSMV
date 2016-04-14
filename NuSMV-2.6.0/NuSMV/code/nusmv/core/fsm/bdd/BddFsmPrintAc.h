3ca297b6d9d998c8bb8535ade670a4b9dacc4e2a
rickyheinitz <afheinitz@zedat.fu-berlin.de>
2016-04-14 20:15:22 +0200

    BddFsmPrint_interesting_states extra

---
+++ NuSMV-2.6.0/NuSMV/code/nusmv/core/fsm/bdd/BddFsmPrintAc.h
@@ -0,0 +1,6 @@
+void BddFsm_print_interesting_states_info(const BddFsm_ptr self, 
+					bdd_ptr interesting_states,
+                                        const boolean print_states,
+                                        const boolean print_defines,
+                                        const boolean print_formula,
+                                        OStream_ptr file);
