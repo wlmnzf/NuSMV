3fcc71aa5d98505d6e9beb854035187349102b80
HannesK <hannes.klarner@fu-berlin.de>
2016-04-15 18:40:48 +0200

    Kleinkram
     - print_states in print_accepting_states umbenannt.
     - print_bdd_size auskommentiert wegen Fehler

--- NuSMV-2.6.0/NuSMV/code/nusmv/core/mc/mcMc.c
+++ NuSMV-2.6.0/NuSMV/code/nusmv/core/mc/mcMc.c
@@ -227,9 +227,7 @@ void Mc_CheckCTLSpec(NuSMVEnv_ptr env, Prop_ptr prop)
     init_and_accepted = bdd_dup(init);
     bdd_and_accumulate(dd, &init_and_accepted, accepted);       
     
-    print_states(env, prop, dd, enc, opts, streams, "\nInitial States: ", init, accepted, init_and_accepted);
-//     print_states(env, prop, opts, streams, "\nInitial States: ", init, accepted, init_and_accepted);
-//     try_to_print_this(env, prop, dd, enc, init, accepted, init_and_accepted, streams);
+    print_accepting_states(env, prop, dd, enc, opts, streams, "\nInitial States: ", init, accepted, init_and_accepted);
     
     bdd_free(dd,init);
     bdd_free(dd,init_and_accepted);
