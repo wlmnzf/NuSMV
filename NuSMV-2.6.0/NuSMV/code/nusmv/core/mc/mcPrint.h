f9153048282c3a6ba6e91fec0756307d9a8e447b
rickyheinitz <afheinitz@zedat.fu-berlin.de>
2016-04-19 10:42:29 +0200

    kommentare versuch 1

--- NuSMV-2.6.0/NuSMV/code/nusmv/core/mc/mcPrint.h
+++ NuSMV-2.6.0/NuSMV/code/nusmv/core/mc/mcPrint.h
@@ -1,3 +1,39 @@
+/* ---------------------------------------------------------------------------
+
+
+  This file is part of NuSMV version 2. Copyright (C) 2016 by FBK-irst. 
+
+  NuSMV version 2 is free software; you can redistribute it and/or 
+  modify it under the terms of the GNU Lesser General Public 
+  License as published by the Free Software Foundation; either 
+  version 2 of the License, or (at your option) any later version.
+
+  NuSMV version 2 is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public 
+  License along with this library; if not, write to the Free Software 
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+  For more information on NuSMV see <http://nusmv.fbk.eu>
+  or email to <nusmv-users@fbk.eu>.
+  Please report bugs to <nusmv-users@fbk.eu>.
+
+  To contact the NuSMV development board, email to <nusmv@fbk.eu>. 
+
+-----------------------------------------------------------------------------*/
+
+/*!
+  \author Sarah Nee, Hannes Klarner, Frederike Heinitz, Mehmet Can
+  \brief Header file for print accepting states addon.
+  
+  Print function for printing initial, accepting and initial accepting 
+  states when the commandline option `-a` is specified. 
+
+*/
+
 void print_accepting_states(NuSMVEnv_ptr env,
 		  Prop_ptr prop,
 		  DDMgr_ptr dd,
