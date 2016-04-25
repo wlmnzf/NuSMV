cf4323bf8369f1b7dea2a143b5f2874c3837f51f
memocan32 <memocan_32@hotmail.de>
2016-04-25 11:41:54 +0200

    CTL Formeln erzeugt

--- Mehmet/CTL.py
+++ Mehmet/CTL.py
@@ -1,25 +1,28 @@
 from PyBoolNet import FileExchange as FEX
+from PyBoolNet import InteractionGraphs as IGs
 from PyBoolNet import StateTransitionGraphs as STGs
-from PyBoolNet import AttractorDetection as AD
+from PyBoolNet import ModelChecking as MC
 from PyBoolNet import TrapSpaces as TS
+from PyBoolNet import AttractorDetection as AD
+from PyBoolNet import QuineMcCluskey as QMC
+from PyBoolNet import TemporalQueries as TQ
 
-import networkx as nx
+import networkx
 
 
 primes = FEX.bnet2primes("raf_network.bnet")
 update = "asynchronous"
 tspaces = TS.trap_spaces(primes, "min")
+a = ", ".join(STGs.subspace2str(primes, x) for x in tspaces)
 
-stg = STGs.primes2stg(primes, update)
-
-
-STGs.add_style_sccs(stg)
-attractors = AD.compute_attractors_tarjan(primes, stg)
-for A in attractors:
-	print [STGs.state2str(x) for x in A]
-
-
-"""
 print tspaces
-print ", ".join(STGs.subspace2str(primes, x) for x in tspaces)
-"""
+print a
+print TQ.EF_oneof_subspaces(primes,tspaces)
+
+spec = "CTLSPEC " + TQ.EF_oneof_subspaces(primes,tspaces)
+print spec
+
+init = "INIT TRUE"
+answer = MC.check_primes(primes, update, init, spec, False)
+print answer
+
