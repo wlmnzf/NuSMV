545a5a1dc1b61e94caa2d04f831a2fb08e5ec36c
rickyheinitz <afheinitz@zedat.fu-berlin.de>
2016-04-26 13:40:47 +0200

    zeiten in datei schreiben lassen

--- Benchmark/step3.sh
+++ Benchmark/step3.sh
@@ -2,11 +2,10 @@
 #!/bin/bash
 nusmvswp='../NuSMV-2.6.0/NuSMV/build/bin/NuSMV'
 
-
 for file in *.smv ;
 do   
    echo working on $file
    fname=$(basename $file)
-   fnameout=${fname%.*}.out
-   time $nusmvswp -dcx -a $fnameout $file
+   fnameout=${fname%.*}.out 
+   /usr/bin/time -o timeout.txt --append --format " %S %U" $nusmvswp -dcx -a $fnameout $file
 done
