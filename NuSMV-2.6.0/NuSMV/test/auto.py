fb87b8bf44b0cd6e9fb8ae26856b99be0b8987fd
Your Name <you@example.com>
2019-06-18 00:17:47 +0800

    update

--- NuSMV-2.6.0/NuSMV/test/auto.py
+++ NuSMV-2.6.0/NuSMV/test/auto.py
@@ -1,26 +1,26 @@
 import os
 
-# # with open('NoSecure/attack_flush_reload.smv', 'r') as f:
-# with open('SPCache/attack_flush_reload.smv', 'r') as f:
-# # with open('attack_flush_reload.smv', 'r') as f:
-#     smv=f.read()
-#     attacker=["nop","load","store"]
-#     victim=["nop","load","store"]
-#     # attacker=["nop","load","store","flush","branch"]
-#     # victim=["nop","load","store","flush"]
-#     if not os.path.exists("tmp"):
-#         os.mkdir("tmp")
-#
-#     for i in range(len(attacker)):
-#         for j in range(len(victim)):
-#             if(attacker[i]=="branch" and victim[j]=="flush"):
-#                 continue
-#             f_path="tmp/F%d%d.smv" % (i,j)
-#             with open(f_path,'w') as f:
-#                 smv_cpy=smv.replace("{INIT_SC}",'FALSE')
-#                 smv_cpy=smv_cpy.replace("{INIT_ATTACKER}",attacker[i])
-#                 smv_cpy=smv_cpy.replace("{INIT_VICTIM}",victim[j])
-#                 f.write(smv_cpy)
+# with open('NoSecure/attack_flush_reload.smv', 'r') as f:
+with open('SPCache/attack_flush_reload.smv', 'r') as f:
+# with open('attack_flush_reload.smv', 'r') as f:
+    smv=f.read()
+    attacker=["nop","load","store"]
+    victim=["nop","load","store"]
+    # attacker=["nop","load","store","flush","branch"]
+    # victim=["nop","load","store","flush"]
+    if not os.path.exists("tmp"):
+        os.mkdir("tmp")
+
+    for i in range(len(attacker)):
+        for j in range(len(victim)):
+            if(attacker[i]=="branch" and victim[j]=="flush"):
+                continue
+            f_path="tmp/F%d%d.smv" % (i,j)
+            with open(f_path,'w') as f:
+                smv_cpy=smv.replace("{INIT_SC}",'FALSE')
+                smv_cpy=smv_cpy.replace("{INIT_ATTACKER}",attacker[i])
+                smv_cpy=smv_cpy.replace("{INIT_VICTIM}",victim[j])
+                f.write(smv_cpy)
 
 # with open('NoSecure/attack_flush_reload_T.smv', 'r') as f:
 with open('SPCache/attack_flush_reload.smv', 'r') as f:
